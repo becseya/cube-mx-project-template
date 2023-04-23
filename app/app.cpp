5fe31f4faf990927ccc000252d6a8897308755a5
Akos Becsey <becsey.a@gmail.com>
2023-04-23 12:00:07 +0200

    Generate "blink LED"

--- app/app.cpp
+++ app/app.cpp
@@ -1,5 +1,10 @@
 #include "app.hpp"
 
+#include <main.h>
+
 extern "C" void app_init() {}
 
-extern "C" void app_loop() {}
+extern "C" void app_loop() {
+  LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_13);
+  LL_mDelay(500);
+}
