fbe0fcd7d3d347e258836e5701064fb7e884b217
Akos Becsey <becsey.a@gmail.com>
2023-04-27 09:26:40 +0200

    FIX: Includes

--- generated/overlay.mk
+++ generated/overlay.mk
@@ -7,4 +7,11 @@ OBJECTS += ${OVERLAY_OBJECTS}
 
 build/main.o: ../app/build/lib_app.a
 
+FW_PATH_ABS := $(shell echo "${C_SOURCES}" | grep -Po '[^ ]+cube-mx-fw' | grep -Poz '^[^\s]+')
+FW_PATH_RELATIVE := ../tools/.toolchains/cube-mx-fw
+
+C_INCLUDES := $(subst ${FW_PATH_ABS},${FW_PATH_RELATIVE},${C_INCLUDES})
+C_SOURCES := $(subst ${FW_PATH_ABS},${FW_PATH_RELATIVE},${C_SOURCES})
+vpath %.c $(sort $(dir $(C_SOURCES)))
+
 print-%  : ; @echo $($*)
